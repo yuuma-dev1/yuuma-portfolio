fae5417bd9290d027b59d2823b56621e8771e96f
yuuma-dev1 <yuuma.dev1@gmail.com>
2026-02-27 08:49:38 +0900

    Update main.js

--- main.js
+++ main.js
@@ -26,3 +26,9 @@ const observer = new IntersectionObserver((entries) => {
 });
 
 reveals.forEach((el) => observer.observe(el));
+
+const themeBtn = document.getElementById("theme-toggle");
+
+themeBtn.addEventListener("click", () => {
+  document.body.classList.toggle("dark");
+});
